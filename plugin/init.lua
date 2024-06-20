75ce083dc0fbf56ccedc90c3b3494e0d7966c178
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-06-20 17:30:37 +0100

    WIP

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,5 +1,12 @@
 local cause_error = function()
-	vim.api.nvim_buf_set_text(0, 2, 2, 8, 8, { "test", "test", "test", "test", "test", "test" })
+	vim.api.nvim_buf_set_text(
+		0,
+		10,
+		5,
+		14,
+		10,
+		{ "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test" }
+	)
 end
 
 vim.api.nvim_create_user_command("BORK", cause_error, { range = true })
